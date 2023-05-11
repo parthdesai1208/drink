9eb4da70e9fa52bfa8b461c0c1a20a8be829fb5a
parthdesai1208 <dparth78@gmail.com>
2023-05-11 19:44:07 +0530

    add start mixing button in SpecificDrinkScreen.dart

--- lib/SpecificDrink/SpecificDrinkScreen.dart
+++ lib/SpecificDrink/SpecificDrinkScreen.dart
@@ -1,4 +1,3 @@
-
 import 'package:drink/DrinkData.dart';
 import 'package:drink/SpecificDrink/DrinkRepository.dart';
 import 'package:drink/SpecificDrink/bloc/DrinkBloc.dart';
@@ -35,30 +34,40 @@ class SpecificDrinkScreenBody extends StatefulWidget {
 class _SpecificDrinkScreenBodyState extends State<SpecificDrinkScreenBody> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: commonBlocProviderForSpecificDrink(drinkId: widget.drinkId,isLargerSize: false));
+    return Scaffold(
+        body: commonBlocProviderForSpecificDrink(
+            drinkId: widget.drinkId, isLargerSize: false),
+        floatingActionButton: Container(
+            alignment: Alignment.bottomCenter,
+            child: ElevatedButton(
+                onPressed: () {},
+                style: const ButtonStyle(
+                    backgroundColor:
+                        MaterialStatePropertyAll<Color>(Colors.yellow)),
+                child: const Text("Start mixing"))));
   }
 }
 
-Widget commonBlocProviderForSpecificDrink({required String drinkId,required bool isLargerSize}){
+Widget commonBlocProviderForSpecificDrink(
+    {required String drinkId, required bool isLargerSize}) {
   return BlocProvider(
-      create: (context) => DrinkBloc(DrinkRepository(drinkId))
-        ..add(DrinkLoadEvent()),
-      child: BlocBuilder<DrinkBloc, DrinkState>(
-          builder: (context, state) {
-            if (state is DrinkLoadingState) {
-              return const Center(child: CircularProgressIndicator());
-            } else if (state is DrinkLoadedState) {
-              if(isLargerSize){
-                return drinkScreenForDialog(state.drinkClass,context);
-              }else{
-                return drinkScreen(state.drinkClass, context);
-              }
-            } else if (state is DrinkErrorState) {
-              return Center(child: Text(state.error));
-            } else {
-              return Container();
-            }
-          }));
+      create: (context) =>
+          DrinkBloc(DrinkRepository(drinkId))..add(DrinkLoadEvent()),
+      child: BlocBuilder<DrinkBloc, DrinkState>(builder: (context, state) {
+        if (state is DrinkLoadingState) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (state is DrinkLoadedState) {
+          if (isLargerSize) {
+            return drinkScreenForDialog(state.drinkClass, context);
+          } else {
+            return drinkScreen(state.drinkClass, context);
+          }
+        } else if (state is DrinkErrorState) {
+          return Center(child: Text(state.error));
+        } else {
+          return Container();
+        }
+      }));
 }
 
 Widget drinkScreen(DrinkClass drinkClass, BuildContext context) {
@@ -111,11 +120,12 @@ Widget drinkScreen(DrinkClass drinkClass, BuildContext context) {
 
   return SingleChildScrollView(
     child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-      Stack(fit: StackFit.passthrough,children: [
+      Stack(fit: StackFit.passthrough, children: [
         Image.network(drinkClass.drinks![0].strDrinkThumb!,
             height: MediaQuery.of(context).size.height * 0.50,
             width: double.maxFinite,
-            fit: BoxFit.cover, loadingBuilder: (context, child, loadingProgress) {
+            fit: BoxFit.cover,
+            loadingBuilder: (context, child, loadingProgress) {
           if (loadingProgress == null) {
             return child;
           }
@@ -131,17 +141,26 @@ Widget drinkScreen(DrinkClass drinkClass, BuildContext context) {
         Positioned(
             left: 16,
             top: 36,
-            child: Container(decoration: const ShapeDecoration(color: Colors.grey,shape: CircleBorder()),
-              child: IconButton(padding: const EdgeInsets.all(0),
+            child: Container(
+              decoration: const ShapeDecoration(
+                  color: Colors.grey, shape: CircleBorder()),
+              child: IconButton(
+                  padding: const EdgeInsets.all(0),
                   onPressed: () {
                     Navigator.pop(context);
                   },
                   icon: const Icon(Icons.close, size: 32)),
             )),
-        Positioned(left: 16,bottom: 16,right: 0,
-          child: SizedBox(width: double.maxFinite,
-              child: Text(drinkClass.drinks![0].strDrink!
-                ,style: TextStyle(fontSize: 32,fontWeight: FontWeight.bold),)),
+        Positioned(
+          left: 16,
+          bottom: 16,
+          right: 0,
+          child: SizedBox(
+              width: double.maxFinite,
+              child: Text(
+                drinkClass.drinks![0].strDrink!,
+                style: const TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+              )),
         )
       ]),
       const Padding(
@@ -159,7 +178,7 @@ Widget drinkScreen(DrinkClass drinkClass, BuildContext context) {
               return Text(ingredientList[index]);
             },
             itemCount: ingredientList.length),
-      )
+      ),
     ]),
   );
 }
