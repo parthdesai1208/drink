9bb8c8b40bf72109edb1fd1a5fa5896ccf410c69
parthdesai1208 <dparth78@gmail.com>
2023-05-12 16:14:46 +0530

    make UI for BottomSheet Steps for mobile & tablet

--- lib/SpecificDrink/SpecificDrinkScreen.dart
+++ lib/SpecificDrink/SpecificDrinkScreen.dart
@@ -5,8 +5,11 @@ import 'package:drink/SpecificDrink/bloc/DrinkEvent.dart';
 import 'package:drink/SpecificDrink/bloc/DrinkState.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:responsive_builder/responsive_builder.dart';
 
 import '../SpecificCategory/specific_category_screen.dart';
+
+var _drinkClass = DrinkClass();
 
 class SpecificDrinkScreen extends StatelessWidget {
   const SpecificDrinkScreen({Key? key, required this.drinkId})
@@ -34,17 +37,46 @@ class SpecificDrinkScreenBody extends StatefulWidget {
 class _SpecificDrinkScreenBodyState extends State<SpecificDrinkScreenBody> {
   @override
   Widget build(BuildContext context) {
+    var isLightTheme = false;
+    if (MediaQuery.of(context).platformBrightness == Brightness.light) {
+      isLightTheme = true;
+    }
+    Color buttonColor;
+    if (isLightTheme) {
+      buttonColor = Colors.orange;
+    } else {
+      buttonColor = Colors.orangeAccent;
+    }
     return Scaffold(
         body: commonBlocProviderForSpecificDrink(
             drinkId: widget.drinkId, isLargerSize: false),
         floatingActionButton: Container(
             alignment: Alignment.bottomCenter,
-            child: ElevatedButton(
-                onPressed: () {},
-                style: const ButtonStyle(
-                    backgroundColor:
-                        MaterialStatePropertyAll<Color>(Colors.yellow)),
-                child: const Text("Start mixing"))));
+            child: ResponsiveBuilder(
+              builder:
+                  (BuildContext context, SizingInformation sizingInformation) {
+                return ElevatedButton(
+                    onPressed: () {
+                      if (sizingInformation.isMobile ||
+                          sizingInformation.isTablet) {
+                        openBottomSheet(
+                            context: context, drinkClass: _drinkClass);
+                      } else {}
+                    },
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: buttonColor,
+                        shape: const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.horizontal(
+                                left: Radius.circular(32),
+                                right: Radius.circular(32)))),
+                    child: const Padding(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 24, vertical: 8),
+                      child:
+                          Text("Start mixing", style: TextStyle(fontSize: 20)),
+                    ));
+              },
+            )));
   }
 }
 
@@ -57,6 +89,7 @@ Widget commonBlocProviderForSpecificDrink(
         if (state is DrinkLoadingState) {
           return const Center(child: CircularProgressIndicator());
         } else if (state is DrinkLoadedState) {
+          _drinkClass = state.drinkClass;
           if (isLargerSize) {
             return drinkScreenForDialog(state.drinkClass, context);
           } else {
@@ -159,7 +192,8 @@ Widget drinkScreen(DrinkClass drinkClass, BuildContext context) {
               width: double.maxFinite,
               child: Text(
                 drinkClass.drinks![0].strDrink!,
-                style: const TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+                style:
+                    const TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
               )),
         )
       ]),
@@ -181,4 +215,73 @@ Widget drinkScreen(DrinkClass drinkClass, BuildContext context) {
       ),
     ]),
   );
+}
+
+Future openBottomSheet(
+    {required BuildContext context, required DrinkClass drinkClass}) {
+  return showModalBottomSheet(
+      constraints:
+          BoxConstraints(minHeight: MediaQuery.of(context).size.height * 0.95),
+      barrierColor: Colors.transparent.withOpacity(0.75),
+      isScrollControlled: true,
+      shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(32), topRight: Radius.circular(32))),
+      clipBehavior: Clip.antiAlias,
+      context: context,
+      builder: (context) {
+        var stepList = getSteps(drinkClass: drinkClass);
+        return SingleChildScrollView(
+          child: Column(children: [
+            Image.network(drinkClass.drinks![0].strDrinkThumb!,
+                height: MediaQuery.of(context).size.height * 0.50,
+                width: double.maxFinite,
+                fit: BoxFit.cover,
+                loadingBuilder: (context, child, loadingProgress) {
+              if (loadingProgress == null) {
+                return child;
+              }
+              return SizedBox(
+                  height: MediaQuery.of(context).size.height / 3,
+                  child: Center(
+                      child: CircularProgressIndicator(
+                          value: loadingProgress.expectedTotalBytes != null
+                              ? loadingProgress.cumulativeBytesLoaded /
+                                  loadingProgress.expectedTotalBytes!
+                              : null)));
+            }),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Container(
+                  alignment: Alignment.centerLeft,
+                  child: const Text("Steps",
+                      style: TextStyle(
+                          fontSize: 24, fontWeight: FontWeight.w500))),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 32),
+              child: ListView.builder(
+                  padding: EdgeInsets.zero,
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 16),
+                      child: ListTile(
+                          leading: Text((index + 1).toString(),
+                              style: TextStyle(color: Colors.yellow.shade800)),
+                          title: Text(stepList[index])),
+                    );
+                  },
+                  itemCount: stepList.length),
+            )
+          ]),
+        );
+      });
+}
+
+List<String> getSteps({required DrinkClass drinkClass}) {
+  var list = drinkClass.drinks![0].strInstructions!.trim().split(".");
+  list.removeWhere((element) => element.isEmpty);
+  return list;
 }
